408542ab2be118c77071bdd17a4bd9548bc938d4
David Cukrowicz <david.cukro@gmail.com>
2018-05-15 23:08:08 +0200

    added hashtable header

---
+++ src/hashtable.h
@@ -0,0 +1,21 @@
+#ifndef _hashtable_h
+#define _hashtable_h
+
+typedef struct {
+    struct hashentry_t* next;
+    void* data;
+} hashentry_t;
+
+typedef struct {
+    size_t capacity;
+    size_t count;
+    hashentry_t* entries;
+} hashtable_t;
+
+hashtable_t* ht_new(size_t capacity);
+void ht_free(hashtable_t** ht);
+
+void ht_put(char* key, void* value);
+void* ht_get(char* key);
+
+#endif
